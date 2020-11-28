498bc6813b9ec3911566db74113d3ceccc570e69
kogayama <kogayama0130@gmail.com>
2020-11-29 03:16:39 +0900

    edit devise parameter and js

---
+++ app/assets/javascripts/sign_up.js
@@ -0,0 +1,9 @@
+$(function(){
+  $("#password-check").change(function(){
+    if ($(this).prop('checked')){
+      $('.password').attr('type','text');
+    } else {
+      $('.password').attr('type','password');
+    }
+  });
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  
+  validates :password, presence: true, length: { minimum: 7 }
 end
