723d6a876e819423c116bc5ff50dc84c71e356bf
kogayama <kogayama0130@gmail.com>
2020-12-07 15:56:52 +0900

    edited room show room review

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,10 +1,11 @@
-$(function(){
+document.addEventListener("turbolinks:load", function() {
   $('.js-modal-open').on('click',function(){
-      $('.js-modal').fadeIn();
-      return false;
+    $('.js-modal').fadeIn();
+    return false;
   });
   $('.js-modal-close').on('click',function(){
       $('.js-modal').fadeOut();
       return false;
   });
-});
+  })
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+
+  validates :title, presence: true, length: { minimum: 35 }
+
 end
