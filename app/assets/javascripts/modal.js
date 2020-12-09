45e63e06c775f98fd12f425a92ad44b94ec4c3fa
kogayama <kogayama0130@gmail.com>
2020-12-10 01:48:19 +0900

    save once

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,6 +1,8 @@
 document.addEventListener("turbolinks:load", function() {
   $('.js-modal-open').on('click',function(){
-    $('.js-modal').fadeIn();
+    var target = $(this).data('target');
+    var modal = document.getElementById(target);
+    $(modal).fadeIn();
     return false;
   });
   $('.js-modal-close').on('click',function(){
