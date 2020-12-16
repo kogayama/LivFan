dcb54dbca35e29a6a2fcf6abba131d034b7d4e5c
kogayama <kogayama0130@gmail.com>
2020-12-16 18:28:57 +0900

    save cuz wanna try to change to datepicker

--- app/assets/javascripts/book_form.js
+++ app/assets/javascripts/book_form.js
@@ -1,17 +1,1 @@
 
-
-// $(function () {
-//   $('#datetimepicker7').datetimepicker({
-//     format: 'L'
-//   });
-//   $('#datetimepicker8').datetimepicker({
-//     format: 'L',
-//     useCurrent: false
-//   });
-//   $("#datetimepicker7").on("change.datetimepicker", function (e) {
-//     $('#datetimepicker8').datetimepicker('minDate', e.date);
-//   });
-//   $("#datetimepicker8").on("change.datetimepicker", function (e) {
-//       $('#datetimepicker7').datetimepicker('maxDate', e.date);
-//   });
-// });
