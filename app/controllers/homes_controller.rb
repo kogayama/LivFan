148946425c8027bce1d3c76faf3a95a9eb6d81ed
kogayama <kogayama0130@gmail.com>
2020-12-20 23:02:01 +0900

    edited search function

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,42 @@ class HomesController < ApplicationController
   end
 
   def search
-    
+    binding.pry
+    if params[:search].present? && params[:search].strip != ""
+      session[:location] = params[:search]
+    end
+
+    if session[:location] && session[:location] != ""
+      @rooms_location = Room.where(done: true).nearbys(session[:location], 30, units: :km)
+    else
+      @rooms_location = Room.where(done: true).all
+    end
+
+    @search = @rooms_location.ransack(params[:q])
+    @rooms = @search.result
+
+    @arrRooms = @rooms.to_a
+
+    if (params[:start_date] && params[:end_date] && !params[:start_date].empty? &&  !params[:end_date].empty?)
+
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+
+      @rooms.each do |room|
+
+        is_disable = room.books.where(
+                      "(? <= start_date AND start_date <= ?)
+                      OR (? <= end_date AND end_date <= ?)
+                      OR (start_date < ? AND ? < end_date)",
+                      start_date, end_date,
+                      start_date, end_date,
+                      start_date, end_date
+                    ).limit(1)
+
+        if is_disable.length > 0
+          @arrRooms.delete(room)
+        end
+      end
+    end
   end
 end
