8a9734bf4ab31ea0f6d51a695e4325bc1df8aec1
kogayama <kogayama0130@gmail.com>
2020-12-25 18:57:03 +0900

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Moment
+module LivFan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
