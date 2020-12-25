3998b3c53ffd4e35329fc3927bbee4fed7fe13b1
kogayama <kogayama0130@gmail.com>
2020-12-26 05:39:59 +0900

    added group create show function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,20 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_path(@group.id)
+    else
+      render :new
+    end
   end
 
   def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:concept, :introduction, user_ids: [])
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  
+  accepts_nested_attributes_for :group_users
 
   validates :concept, presence: true, uniqueness: true
 end
