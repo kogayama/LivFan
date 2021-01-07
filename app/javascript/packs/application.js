196d51a5e04ccf6f1fea3fe996ca4059fc3787bc
kogayama <kogayama0130@gmail.com>
2021-01-07 20:45:42 +0900

    edited jquery info and something

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 window.$ = window.jQuery = require('jquery');
-require("jquery.raty")
+require('packs/raty')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
